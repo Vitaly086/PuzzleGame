677fd872c53c49627da09d91f2636d1e12be1cee
Viktoria <viktoriabandura@mail.ru>
2023-11-13 12:55:12 +0300

    add menu screen

--- Assets/Scripts/GlobalConstants.cs
+++ Assets/Scripts/GlobalConstants.cs
@@ -3,5 +3,8 @@ namespace DefaultNamespace
     public static class GlobalConstants
     {
         public const int DICE_FACE_COUNT = 6;
+        public const string BOOTSTRAP_SCENE = "BootstrapScene";
+        public const string MENU_SCENE = "MenuScene";
+        public const string GAME_SCENE = "GameScene";
     }
 }
